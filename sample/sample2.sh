b3b6575fba1440ed8f9adc65c7432e8a052b40fc
iwase22334 <iwase22334@gmail.com>
2019-06-16 14:05:44 +0000

    Documentation: Add sample program

---
+++ sample/sample2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+data=$(cat - <<EOS
+{
+    "race_id": {
+        "year": "2019",
+        "monthday":"0303",
+        "jyocd":"10",
+        "kaiji":"01",
+        "nichiji": "08",
+        "racenum": "12"
+    },
+    "tansyo_vote": [
+        { "umaban": "01", "hyosu": 20 },
+        { "umaban": "05", "hyosu": 4 },
+        { "umaban": "06", "hyosu": 2 }
+    ]
+}
+EOS
+)
+curl -X POST -d "$data" -H "Connection: keep-alive" localhost:8080 -v
