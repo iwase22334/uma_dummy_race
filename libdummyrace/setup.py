c81b0ee7a916a5c69795ab0f8016005ed3b246d0
iwase22334 <iwase22334@gmail.com>
2019-09-18 21:36:54 +0900

    WIP: add necessary file

---
+++ libdummyrace/setup.py
@@ -0,0 +1,10 @@
+setup(
+    name='dummyrace',
+    version='0.0.1',
+    description='Client lib for uma_dummy_race',
+    long_description=readme,
+    author='iwase22334',
+    author_email='iwase22334@gmail.com',
+    url='https://github.com/iwase22334/uma_dummy_race',
+    packages=find_packages(exclude=('tests', 'docs'))
+)
