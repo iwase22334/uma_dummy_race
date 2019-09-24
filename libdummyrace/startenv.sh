8be2c5d9eea5a85ed9913f191bae44ee284b21d3
iwase22334 <iwase22334@gmail.com>
2019-09-24 21:05:54 +0900

    Removal: remove client tool

--- libdummyrace/startenv.sh
+++
@@ -1,8 +0,0 @@
-export PGUSER="postgres"
-export PGPASSWORD="password"
-export PGHOST="192.168.11.2"
-export PGPORT="5433"
-export PGDATABASE_SRC="everydb2"
-export DATABASE_URL_SRC="postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}:${PGPORT}/${PGDATABASE_SRC}"
-export DUMMY_RACE_URL="http://localhost:8080"
-
